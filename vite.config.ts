6153bd29a1b2055f1ef0d886f3647f6e940c177e
Surthi Vaibhav <130393195+vaibhxvvy@users.noreply.github.com>
2025-11-18 00:59:14 +0530

    Configure base path for Vite build
    Set base path for built assets to relative.

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "./", // <<---- ensures built assets use relative paths
   server: {
     host: "::",
     port: 8080,
